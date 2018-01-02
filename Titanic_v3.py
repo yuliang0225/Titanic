56f97c840ff3685cb38d392cc8b67a892b3c434d
Sylar <yuliang0225@sina.com>
2018-01-03 03:30:51 +0900

    Add ver 3.0

--- Titanic_v3.py
+++ Titanic_v3.py
@@ -3,7 +3,47 @@
 """
 Created on Tue Jan  2 18:57:47 2018
 Titanic v3
-@author: smuch
+@author: smuch and LD Freeman
 """
+#%% Load Library
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+#load packages
+import sys #access to system parameters https://docs.python.org/3/library/sys.html
+import pandas as pd #collection of functions for data processing and analysis modeled after R dataframes with SQL like features
+import matplotlib #collection of functions for scientific and publication-ready visualization
+import numpy as np #foundational package for scientific computing
+import scipy as sp #collection of functions for scientific computing and advance mathematics
+import sklearn #collection of machine learning algorithms
+#misc libraries
+import random
+import time
+#ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
+#%% Load Data Modelling Libraries
+#Common Model Algorithms
+from sklearn import svm, tree, linear_model, neighbors, naive_bayes, ensemble, discriminant_analysis, gaussian_process
+from xgboost import XGBClassifier
+
+#Common Model Helpers
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn import feature_selection
+from sklearn import model_selection
+from sklearn import metrics
+
+#Visualization
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+import seaborn as sns
+from pandas.tools.plotting import scatter_matrix
+
+#Configure Visualization Defaults
+#%matplotlib inline = show plots in Jupyter Notebook browser
+#%matplotlib inline
+mpl.style.use('ggplot') # Style Selection
+sns.set_style('white')
+pylab.rcParams['figure.figsize'] = 12,8
 #%%
 
